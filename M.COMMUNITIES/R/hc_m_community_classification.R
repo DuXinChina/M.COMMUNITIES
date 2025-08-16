e3a9fc08b485a84f27e076cab2902e03b79aae97
DuXinChina <71441722+DuXinChina@users.noreply.github.com>
2025-08-16 12:25:38 +0800

    Update hc_m_community_classification.R

--- M.COMMUNITIES/R/hc_m_community_classification.R
+++ M.COMMUNITIES/R/hc_m_community_classification.R
@@ -1,15 +1,15 @@
-#' 基于hc聚类对小群落进行划分
-#' @description 基于hc聚类对小群落进行划分
+#' 基于hc聚类对小群落进行划分 / Divide mirror communities based on HC clustering
+#' @description 基于hc聚类对小群落进行划分 / Divide mirror communities based on HC clustering
 #' @usage hc_m_community_classification(entity,minx,miny,maxx,maxy,scale,edge)
-#' @param entity 植物个体的分布坐标与物种
-#' @param minx 核心区边界，最小x
-#' @param miny 核心区边界，最小y
-#' @param maxx 核心区边界，最大x
-#' @param maxy 核心区边界，最大y
-#' @param scale hc聚类的scale参数
-#' @param edge 基于边界筛选小群落的方法，可选"inside"与"centroid"
+#' @param entity 植物个体的分布坐标与物种 / Distribution coordinates and species of plant individuals
+#' @param minx 核心区边界，最小x / Core area boundary, minimum x
+#' @param miny 核心区边界，最小y / Core area boundary, minimum y
+#' @param maxx 核心区边界，最大x / Core area boundary, maximum x
+#' @param maxy 核心区边界，最大y / Core area boundary, maximum y
+#' @param scale hc聚类的scale参数 / scale parameter of HC clustering
+#' @param edge 基于边界筛选小群落的方法，可选"inside"与"centroid" / Method for screening mirror communities based on boundaries, with optional "inside" and "centroid"
 #'
-#' @return 小群落的划分
+#' @return 小群落的划分 / Division of mirror communities
 #' @export
 #'
 #' @examples
@@ -77,4 +77,5 @@ hc_m_community_classification=function(entity,minx,miny,maxx,maxy,scale,edge)
   result$m_community_class=match(result$m_community_class, sort(unique(result$m_community_class)))
   result
 }
+
 
